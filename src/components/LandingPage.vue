9ab18407dfb3e1c20d7de531c47b9314416e35cc
Carlos Alfredo Castillo Rodriguez <cacr1990@gmail.com>
2023-08-19 23:47:47 -0400

    ADD: instalacion de bootstrap y estilos basicos

---
+++ src/components/LandingPage.vue
@@ -0,0 +1,83 @@
+<template>
+    <div>
+      <header class="jumbotron text-center">
+        <h1>Título de la Aplicación</h1>
+        <p>Subtítulo convincente que resalta los beneficios clave de la aplicación.</p>
+        <button class="btn btn-primary">Prueba Gratis</button>
+      </header>
+  
+      <section class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <h2>Beneficios y Características</h2>
+            <ul>
+              <li>Beneficio 1: Lorem ipsum dolor sit amet.</li>
+              <li>Beneficio 2: Consectetur adipiscing elit.</li>
+              <li>Beneficio 3: Sed do eiusmod tempor incididunt.</li>
+            </ul>
+          </div>
+          <div class="col-md-6">
+            <img src="url_de_la_imagen" alt="Demostración visual" class="img-fluid">
+          </div>
+        </div>
+      </section>
+  
+      <section class="container text-center">
+        <h2>Testimonios de Usuarios</h2>
+        <div class="row">
+          <div class="col-md-4">
+            <img src="url_de_la_imagen" alt="Usuario 1" class="rounded-circle">
+            <p>“Lorem ipsum dolor sit amet, consectetur adipiscing elit.”</p>
+            <p><strong>Usuario 1</strong></p>
+          </div>
+          <div class="col-md-4">
+            <img src="url_de_la_imagen" alt="Usuario 2" class="rounded-circle">
+            <p>“Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”</p>
+            <p><strong>Usuario 2</strong></p>
+          </div>
+          <div class="col-md-4">
+            <img src="url_de_la_imagen" alt="Usuario 3" class="rounded-circle">
+            <p>“Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.”</p>
+            <p><strong>Usuario 3</strong></p>
+          </div>
+        </div>
+      </section>
+  
+      <!-- Otras secciones seguirían aquí -->
+  
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LandingPage',
+  };
+  </script>
+  
+  <style>
+  /* Estilos personalizados para responsividad */
+  @media (max-width: 767px) {
+    /* Estilos para pantallas pequeñas */
+  }
+  
+  @media (min-width: 768px) {
+    /* Estilos para pantallas medianas y más grandes */
+  }
+  
+  /* Estilos generales */
+  body {
+    font-family: 'Arial', sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+  
+  /* Estilos de encabezado */
+  .jumbotron {
+    background-color: #f8f9fa;
+    padding: 60px 0;
+    margin-bottom: 0;
+  }
+  
+  /* Otros estilos según necesidad */
+  </style>
+  
